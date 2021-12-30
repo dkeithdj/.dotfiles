2a8e1cd07ab2428fdd3176e55c2a4bdab3b53ec6
Denrei Keith <denzkei12@gmail.com>
2021-12-30 20:51:07 +0800

    chore(nvim): changed path

--- nvim/.config/nvim/after/ftplugin/java.lua
+++ nvim/.config/nvim/after/ftplugin/java.lua
@@ -1,7 +1,7 @@
 local map = require("dk.utils").buf_mappings
 
 require("jdtls.setup").add_commands()
-require("dk.lsp.java-config").setup()
+require("dk.config.lsp.java-config").setup()
 -- Java-specific
 map("n", "<leader>jo", ':lua require"jdtls".organize_imports()<CR>')
 map("n", "<leader>je", ':lua require("jdtls").extract_variable()<CR>')
